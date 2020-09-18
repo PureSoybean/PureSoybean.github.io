4f87ee192f50b301955da04d84aa22967071a5b6
PureSoybean <51090366@qq.com>
2020-09-18 22:09:31 +0800

    '鏇存柊'

---
+++ autosync.bat
@@ -0,0 +1,15 @@
+@echo off
+REM %DATE:~0,10%  2017/07/06
+set dd=%DATE:~0,10%
+set tt=%time:~0,8%
+set hour=%tt:~0,2%
+echo =======================================================
+echo          Starting automatic git commit push
+echo =======================================================
+REM change file directory
+cd C:\Users\zhan8\OneDrive\github\PureSoybean.github.io\TeXt
+REM start git script 
+echo %~dp0
+git status
+git add . && git commit -m '更新' && git push origin master
+pause
